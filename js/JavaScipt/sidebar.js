8b10f3020699385614c7e1986fb6caa44928e57e
mahyargigi <mahyarghoraian@gmail.com>
2015-04-30 01:19:53 +0430

    primary userslist

--- js/JavaScipt/sidebar.js
+++ js/JavaScipt/sidebar.js
@@ -6,9 +6,29 @@ $(document).ready(function(){
        $(this).removeClass('btn-info');
        $(this).addClass('btn-success');
        $(this).text("following");
+       $(this).removeClass('follow');
+       $(this).addClass('following');
 //       $(this).parent().parent().delay(2000).hide();
 //       window.setTimeout($(this).parent().parent().hide(),2000);
        // ba takhir hide nemishe!
 //       )
    }) ;
+//    $('.following').hover(function(){
+//        console.log("hover");
+//        $(this).removeClass('btn-success');
+//        $(this).addClass('btn-danger');
+//        $(this).text("Unfollow");
+//    });
+//    $('.btn-danger').hover(function(){
+//
+//    });
+//
+//    $('.btn-danger').click(function(){
+//       $(this).removeClass('btn-danger');
+//       $(this).addClass('btn-info');
+//       $(this).text("follow");
+//       $(this).removeClass('following');
+//       $(this).addClass('follow');
+//    });
+
 });
