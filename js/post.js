e674950e5737a790e7324a70672d83279d236331
mahyargigi <mahyarghoraian@gmail.com>
2015-05-02 04:29:45 +0430

    sidebar+userlist+post changed + test.html applied

--- js/post.js
+++ js/post.js
@@ -1,16 +1,30 @@
 /**
  * Created by User on 5/1/15.
  */
-$(document).ready(function(){
-    expandTextarea('textarea');
-});
 function expandTextarea(id) {
-        console.log("hii");
+    console.log("hii");
     var $element = $('textarea').get(0);
 
-    $element.addEventListener('keyup', function() {
+    $element.addEventListener('keyup', function () {
         this.style.overflow = 'hidden';
         this.style.height = 0;
         this.style.height = this.scrollHeight + 'px';
     }, false);
 }
+
+$(document).ready(function () {
+    expandTextarea('textarea');
+    $('#comment-btn').on('click',
+        function () {
+            console.log($('textarea').val());
+            if ($('textarea').val() !== '') {
+                var comment = $('textarea').val();
+                $('<li><div class="list-group-item"><div class="row-picture"><img class="circle" src="Img/mahyar2.jpg" alt="icon"></div><div class="row-content"><h4 class="list-group-item-heading">Mahyar Gigi</h4><p class="list-group-item-text">'+comment+'</p></div></div><!--</div>--><div class="list-group-separator"></div></li>').insertBefore('.text-button');
+                $('textarea').val('');
+            }
+        });
+});
+
+
+
+
