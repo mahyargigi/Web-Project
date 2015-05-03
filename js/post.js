b29131b6948c10caa40ace92fe924a16b75b2cba
mahyargigi <mahyarghoraian@gmail.com>
2015-05-03 16:03:03 +0430

    follow/unfollow+tabs+adding another user profile for for anonymous user+some bugs fixed in post&timeline

--- js/post.js
+++ js/post.js
@@ -14,13 +14,21 @@ function expandTextarea(id) {
 
 $(document).ready(function () {
     expandTextarea('textarea');
-    $('#comment-btn').on('click',
+
+    $('#comment-btn1').on('click',
         function () {
-            console.log($('textarea').val());
-            if ($('textarea').val() !== '') {
-                var comment = $('textarea').val();
-                $('<li><div class="list-group-item"><div class="row-picture"><img class="circle" src="Img/mahyar2.jpg" alt="icon"></div><div class="row-content"><h4 class="list-group-item-heading">Mahyar Gigi</h4><p class="list-group-item-text">'+comment+'</p></div></div><!--</div>--><div class="list-group-separator"></div></li>').insertBefore('.text-button');
-                $('textarea').val('');
+            if ($('#focusedInput1').val() !== '') {
+                var comment = $('#focusedInput1').val();
+                $('<li><div class="list-group-item"><div class="row-picture"><img class="circle" src="Img/mahyar2.jpg" alt="icon"></div><div class="row-content"><h4 class="list-group-item-heading">Mahyar Gigi</h4><p class="list-group-item-text">'+comment+'</p></div></div><!--</div>--><div class="list-group-separator"></div></li>').insertBefore('#text-button1');
+                $('#focusedInput1').val('');
+            }
+        });
+    $('#comment-btn2').on('click',
+        function () {
+            if ($('focusedInput2').val() !== '') {
+                var comment = $('#focusedInput2').val();
+                $('<li><div class="list-group-item"><div class="row-picture"><img class="circle" src="Img/mahyar2.jpg" alt="icon"></div><div class="row-content"><h4 class="list-group-item-heading">Mahyar Gigi</h4><p class="list-group-item-text">'+comment+'</p></div></div><!--</div>--><div class="list-group-separator"></div></li>').insertBefore('#text-button2');
+                $('#focusedInput2').val('');
             }
         });
 });
